15773c20d65d911fbb2bdbc9077a9f07456ada0e
Kenji <kenji.au9@gmail.com>
2020-10-12 19:43:18 -0600

    Updates

---
+++ precache-manifest.ad9b7d49be35258ae2123ccd6d5d2810.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f461c22bdbfae3df7e3e26c51a37f6f9",
+    "url": "/TECHCareers-React-Redux-Milestone/index.html"
+  },
+  {
+    "revision": "3a685b465435d1898ec5",
+    "url": "/TECHCareers-React-Redux-Milestone/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "df3d04a40d6ad0fdcb26",
+    "url": "/TECHCareers-React-Redux-Milestone/static/css/main.04ec16dd.chunk.css"
+  },
+  {
+    "revision": "3a685b465435d1898ec5",
+    "url": "/TECHCareers-React-Redux-Milestone/static/js/2.c223a886.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/TECHCareers-React-Redux-Milestone/static/js/2.c223a886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3d04a40d6ad0fdcb26",
+    "url": "/TECHCareers-React-Redux-Milestone/static/js/main.79f613d2.chunk.js"
+  },
+  {
+    "revision": "558f053e8ebf13076394",
+    "url": "/TECHCareers-React-Redux-Milestone/static/js/runtime-main.f2cf965b.js"
+  },
+  {
+    "revision": "5f4ac94578795f361eb913fe9b56b096",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/Banner.5f4ac945.jpg"
+  },
+  {
+    "revision": "142ce775ef48393e466584554b46e088",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/EventImageFour.142ce775.jpg"
+  },
+  {
+    "revision": "003f006cbf4b8e758725030a9b8047de",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/EventImageOne.003f006c.jpg"
+  },
+  {
+    "revision": "527df372ae8b4aba8dc1c1ea9c48d731",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/EventImageThree.527df372.jpg"
+  },
+  {
+    "revision": "7eb48186b61a76c90675ae827a739f2b",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/EventImageTwo.7eb48186.jpg"
+  },
+  {
+    "revision": "c9e0e02c6c3515ea4042c29230d3a74b",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/LandingPage.c9e0e02c.jpg"
+  },
+  {
+    "revision": "1f15ab778d085100e5f81c06ff6425e0",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/NewsImageFour.1f15ab77.jpg"
+  },
+  {
+    "revision": "d511dccf3a414f78de49607d07985326",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/NewsImageOne.d511dccf.jpg"
+  },
+  {
+    "revision": "bf17700a9af7959987ca7bf34c77159e",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/NewsImageThree.bf17700a.jpg"
+  },
+  {
+    "revision": "44ab5ede5b61f92ebb6c27def69fdfdb",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/NewsImageTwo.44ab5ede.jpg"
+  },
+  {
+    "revision": "5755b1f61de5eba1c830032c9d0cf80f",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/about-us.5755b1f6.jpg"
+  },
+  {
+    "revision": "e52f0cb3d26ae230e23181a1a46df819",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/faq.e52f0cb3.jpg"
+  },
+  {
+    "revision": "2c3c507780ecf8914e432fa78497e7f7",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/logo-final.2c3c5077.png"
+  },
+  {
+    "revision": "23f27765d1bb07de6c70c7785a16aafb",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/mission.23f27765.jpg"
+  },
+  {
+    "revision": "a2d43d4e28d76d26069c93c13c10cb07",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/thankyou.a2d43d4e.jpg"
+  },
+  {
+    "revision": "40c3be3cef2104378b140aa3bd047b10",
+    "url": "/TECHCareers-React-Redux-Milestone/static/media/vision.40c3be3c.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/TECHCareers-React-Redux-Milestone/precache-manifest.ad9b7d49be35258ae2123ccd6d5d2810.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TECHCareers-React-Redux-Milestone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
